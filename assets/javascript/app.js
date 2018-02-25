9060be517df6b82e9c185f49f2c7cd009ef93613
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-25 12:38:56 -0800

    added a background and fixed the answers array so that the correct answer is in a random array position everyt time also added a restart button and correct and incorrect counters

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
   var intervalId;
   var timer = 10;
   var index;
+  var rand;
   var isEnd = false;
-  var length;
-  var endCount = 0;
-  var isTimeUp = false;
+  var correctCount = 0;
+  var incorrectCount = 0;
+
   initializeArray();
 
 
@@ -20,30 +21,19 @@ $(document).ready(function() {
         questions = response.results;
         console.log(questions);
       });
-    // questions = [{text:"What color is the sky?",
-    //               incorrect_answers: ["white", "blue", "green", "lemonchiffon"],
-    //               answer: "blue",
-    //               answered: false
-    //              },
-    //              {text:"What color is the ground?",
-    //               incorrect_answers: ["white", "blue", "green", "lemonchiffon"],
-    //               answer: "blue",
-    //               answered: false
-    //              },
-    //              {text:"What color is the tree?",
-    //               incorrect_answers: ["white", "blue", "green", "lemonchiffon"],
-    //               answer: "blue",
-    //               answered: false
-    //              }
-    // ];
     length = questions.length;
     $("#questions").html("<button id='start' \
       class='startButton'>Start</button>");
   }
+
   function initialize() {
     if(questions.length < 1){
       clearInterval(intervalId);
-      $("#questions").html("Out of Questions.")
+      $("#questions").html("Out of Questions. <br>");
+      $("#questions").append("Correct: " + correctCount + "<br>");
+      $("#questions").append("Incorrect: " + incorrectCount+ "<br>");
+      $("#questions").append("<button id='restart' \
+        class='restart'>Restart</button>");
     }
     else {
       if(isEnd){
@@ -54,9 +44,10 @@ $(document).ready(function() {
       }
       index = Math.floor(Math.random()*questions.length);
       randQuestion = questions[index];
+      rand = Math.floor(Math.random()*randQuestion.incorrect_answers.length);
       questions.splice(index, 1);
-      console.log(questions);
-      randQuestion.incorrect_answers.push(randQuestion.correct_answer);
+      randQuestion.incorrect_answers.splice(rand,
+        0,randQuestion.correct_answer);
       $("#questions").html(randQuestion.question);
       for (var i = 0; i < randQuestion.incorrect_answers.length; i++) {
         $("#questions").append("<br> <button id='options' \
@@ -74,6 +65,7 @@ $(document).ready(function() {
       $("#questions").html("times up!!!");
       $("#questions").append("Correct answer: "
       + randQuestion.correct_answer);
+      incorrectCount++;
       timesUp();
     }
 
@@ -104,15 +96,26 @@ $(document).ready(function() {
     opt = $(this).text();
     if(opt === randQuestion.correct_answer){
       $("#questions").html("CORRECT!!!");
+      correctCount++;
       timesUp();
     }
     else {
       $("#questions").html("INCORRECT <br>");
       $("#questions").append("Correct answer: "
       + randQuestion.correct_answer);
+      incorrectCount++;
       timesUp();
     }
   });
 
+  $("#questions").on("click","button.restart", function () {
+    timer = 10;
+    isEnd = false;
+    intervalId = setInterval(decrement, 1000);
+    correctCount = 0;
+    incorrectCount = 0;
+    initializeArray();
+    initialize();
+  });
 
 });
